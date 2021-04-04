3bc74ec3cdd20cae22ea6deb7eb701afe18c4b06
Steve Purcell <steve@sanityinc.com>
2021-04-04 14:18:31 +1200

    Make a little more robust

--- flymake-flycheck.el
+++ flymake-flycheck.el
@@ -6,7 +6,7 @@
 ;; Keywords: convenience, languages, tools
 ;; Homepage: https://github.com/purcell/flymake-flycheck
 ;; Version: 0.1-pre
-;; Package-Requires: ((flycheck "31") (emacs "24.1"))
+;; Package-Requires: ((flycheck "31") (emacs "26.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -51,32 +51,43 @@
 (require 'flycheck)
 (require 'flymake)
 
+(defvar flymake-flycheck-debug nil
+  "When non-nil, output some debug messages.")
+
+(defun flymake-flycheck--debug (msg &rest args)
+  "Outputs debug messages if `flymake-flycheck-debug' is non-nil.
+MSG and ARGS are passed to `message'."
+  (when flymake-flycheck-debug
+    (apply 'message (concat "[flymake-flycheck] " msg) args)))
+
+;;;###autoload
 (defun flymake-flycheck-diagnostic-function-for (checker)
   "Wrap CHECKER to make a `flymake-diagnostics-functions' backend."
-  (lambda (report-fn &rest _)
-    (when (flycheck-running-p)
-      (flycheck-syntax-check-interrupt flycheck-current-syntax-check))
-    ;; Based on `flycheck-start-current-syntax-check'
-    (setq flycheck-current-syntax-check
-          (flycheck-syntax-check-new
-           :buffer (current-buffer)
-           :checker checker
-           :context nil
-           :working-directory (flycheck-compute-working-directory checker)))
-    (message "start syntax check")
-    (flycheck-syntax-check-start
-     flycheck-current-syntax-check
-     (lambda (status &optional data)
-       (message "receive status %S" status)
-       (pcase status
-         ('errored (funcall report-fn
-                            :panic
-                            :explanation (format "flycheck checker %s reported error %S" checker data)))
-         ('finished (funcall report-fn
-                             (mapcar #'flymake-flycheck--translate-error data)
-                             :region (cons (point-min) (point-max))))
-         ('interrupted (message "flycheck checker %s reported being interrupted %S" checker data))
-         ('suspcicious (message "flycheck checker %s reported suspicious result %S" checker data)))))))
+  (apply-partially #'flymake-flycheck--diagnostic-function checker))
+
+(defun flymake-flycheck--diagnostic-function (checker report-fn &rest _)
+  "Run CHECKER passing its translated results to a Flymake REPORT-FN."
+  ;; TODO: interrupt any check currently running for this checker,
+  ;; using `flycheck-syntax-check-interrupt'
+  (flymake-flycheck--debug "start syntax check for %s" checker)
+  (flycheck-syntax-check-start
+   (flycheck-syntax-check-new
+    :buffer (current-buffer)
+    :checker checker
+    :context nil
+    :working-directory (flycheck-compute-working-directory checker))
+   (lambda (status &optional data)
+     (flymake-flycheck--debug "received status %S from %s" status checker)
+     (pcase status
+       ('errored (funcall report-fn
+                          :panic
+                          :explanation (format "Flycheck checker %s reported error %S" checker data)))
+       ('finished (funcall report-fn
+                           (mapcar #'flymake-flycheck--translate-error data)
+                           :region (cons (point-min) (point-max))))
+       ('interrupted (flymake-flycheck--debug "checker %s reported being interrupted %S" checker data))
+       ('suspicious (flymake-flycheck--debug "checker %s reported suspicious result %S" checker data))
+       (_ (flymake-flycheck--debug "unexpected status from checker %s: %S" checker status))))))
 
 (defun flymake-flycheck--translate-error (err)
   "Translate flycheck error ERR into a flymake diagnostic."
@@ -88,12 +99,14 @@
      (pcase (flycheck--exact-region err)
        (`(,beg . ,end) end)
        (t (1+ (flycheck-error-pos err))))
-     (flycheck-error-level err)
+     (pcase (flycheck-error-level err)
+       ('error 'flymake-error)
+       ('warning 'flymake-warning)
+       ('info 'flymake-note)
+       (_
+        (flymake-flycheck--debug "Translating unknown error level %s to note" (flycheck-error-level err))
+        'flymake-note))
      (flycheck-error-message err))))
-
-(put 'note 'flymake-category 'flymake-note)
-(put 'warning 'flymake-category 'flymake-warning)
-(put 'error 'flymake-category 'flymake-error)
 
 
 (provide 'flymake-flycheck)
